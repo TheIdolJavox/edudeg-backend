4613d8acf72373b2fce92a057d7c8f3ae25e035e
TheIdolJavox <142756684+TheIdolJavox@users.noreply.github.com>
2025-05-07 00:30:18 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -14,15 +14,13 @@ from sklearn.impute import SimpleImputer
 app = FastAPI()
 port = int(os.environ.get("PORT", 8000))
 
-# Ruta básica para verificar si la API está activa
 @app.get("/")
 def read_root():
     return {"message": "API de EdUdeG funcionando correctamente"}
 
-# Configurar CORS para permitir peticiones desde el dominio
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://edudeg.com"],  # Doominio del sitio
+    allow_origins=["https://edudeg.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -31,12 +29,10 @@ app.add_middleware(
 @app.post("/procesar_csv")
 async def procesar_csv(file: UploadFile = File(...)):
     try:
-        # Leer el contenido del archivo CSV
         content = await file.read()
         content_str = content.decode("utf-8")
         df = pd.read_csv(StringIO(content_str))
 
-        # Verificar que tenga las columnas necesarias
         required_columns = ['ID Resultado', 'Fecha', 'Nombre de Usuario', 'Tema', 'Total Preguntas', 'Errores', '% Errores']
         if not all(col in df.columns for col in required_columns):
             return JSONResponse(
@@ -44,50 +40,41 @@ async def procesar_csv(file: UploadFile = File(...)):
                 content={"error": "El CSV no contiene las columnas necesarias."}
             )
 
-        # Imprimir CSV
-        print(df.isnull().sum())
-        
-        # Convertir la columna 'Fecha' a tipo datetime
         df['Fecha'] = pd.to_datetime(df['Fecha'], format='%d/%m/%Y', errors='coerce')
-
-        # Extraer características temporales
         df['año'] = df['Fecha'].dt.year
         df['mes'] = df['Fecha'].dt.month
         df['día'] = df['Fecha'].dt.day
         df['día_semana'] = df['Fecha'].dt.weekday
 
-        # Limpiar y convertir el porcentaje de errores a decimal
         df['% Errores'] = df['% Errores'].str.replace('%', '', regex=False).astype(float) / 100.0
 
-        # Preparar los datos para el modelo
         X = df[['Total Preguntas', 'Errores', 'año', 'mes', 'día']]
-        y = df['% Errores'].fillna(df['% Errores'].mean())  # Imputar NaN de la columna de errores
+        y = df['% Errores'].fillna(df['% Errores'].mean())
 
-        # Imputar valores faltantes en X (si los hubiera)
         imputer = SimpleImputer(strategy="mean")
         X_imputado = imputer.fit_transform(X)
 
-        # División de los datos en conjunto de entrenamiento y prueba
         X_train, X_test, y_train, y_test = train_test_split(X_imputado, y, test_size=0.2, random_state=42)
 
-        # Asegurarse de que % Errores esté correctamente formateado
-        y_train = y_train.fillna(y_train.mean())  # En caso de que haya NaN en y_train
-        y_test = y_test.fillna(y_test.mean())  # En caso de que haya NaN en y_test
-
-        # Entrenar el modelo
         model = RandomForestRegressor(n_estimators=100, random_state=42)
         model.fit(X_train, y_train)
 
-        # Evaluación
         y_pred = model.predict(X_test)
         mse = mean_squared_error(y_test, y_pred)
         r2 = r2_score(y_test, y_pred)
 
-        # Predecir para todo el dataset
         df['predicciones'] = model.predict(X_imputado)
 
-        # Preparar datos para enviar
-        predicciones = df[['ID Resultado', 'Nombre de Usuario', 'Tema', 'predicciones']].to_dict(orient="records")
+        df_grouped = df.groupby(['Nombre de Usuario', 'Tema']).agg({
+            'predicciones': 'mean',
+            'ID Resultado': 'first',
+            'Tema': 'count'
+        }).rename(columns={'Tema': 'intentos'}).reset_index()
+
+        # Ordenar por nombre de usuario y tema
+        df_grouped = df_grouped.sort_values(by=['Nombre de Usuario', 'Tema'])
+
+        predicciones = df_grouped[['ID Resultado', 'Nombre de Usuario', 'Tema', 'predicciones', 'intentos']].to_dict(orient="records")
 
         return {
             "mse": mse,
